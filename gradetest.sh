b5e6301c32d2592a62e4faed34cbf593cebbb2fb
Colby Williams <ColbyLWilliams@gmail.com>
2019-09-19 14:44:35 -0400

    Update gradetest.sh

--- gradetest.sh
+++ gradetest.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 echo "****"
-
 echo "****"
 echo "****"
 echo "**** Pulling changes from gradetest.sh"
 echo "****"
 echo "****"
-
+echo "**** Boom"
+echo "****"
+echo "****"
 echo "****"
